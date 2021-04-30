824f1d3990a15790b233795e840d8c63b0c05341
isabella-cruz-ifood <isabella.cruz@ifood.com.br>
2021-04-30 13:48:19 -0300

    Making code more efficiently

--- app/src/main/java/com/isabella/diceroller/MainActivity.kt
+++ app/src/main/java/com/isabella/diceroller/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import kotlin.random.Random
 
-
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//      Finding my View efficiently with lateinit initialization
+        diceImage = findViewById(R.id.dice_image)
 
 //        Getting reference to the button and assing it to an immutable variable called rollButton
         val rollButton = findViewById<Button>(R.id.roll_button)
@@ -23,8 +26,6 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 //        Write have the rollDice method to get a random number between 1 and 6
         val randomInt = Random.nextInt(6) + 1
-//        Use findViewById to get a reference to the TextView with the ImageView
-        val diceImage = findViewById<ImageView>(R.id.dice_image)
 //       Choose the right drawable resource based on the value of randomInt
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
